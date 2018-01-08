f8c6797ba28451c60e33c40283460a57b4a3d818
yuanzh <yuanzh@yuanzhdeMacBook-Pro-5.local>
2018-01-08 18:42:54 +0800

    [Init]

---
+++ index.js
@@ -0,0 +1,70 @@
+const fs = require('fs');
+const uglifyJs = require("uglify-js").minify;
+const babel = require('babel-core');
+const path = require('path');
+
+const defaultOpt = {
+  filepath: null,
+  inject: null,
+  uglify: true,
+  babelpath: null,
+}
+
+function HtmlSetContent(options) {
+  Object.assign(this, defaultOpt, options);
+}
+
+HtmlSetContent.prototype.intype = {
+  '.js': 'script',
+  '.css': 'style'
+}
+
+HtmlSetContent.prototype.mergeText = (inject, html, content, intype) => {
+  const result = new RegExp(`</${inject}>`, 'g');
+
+  return html.replace(result, `<${intype}>${content}</${intype}></${inject}>`);
+}
+
+HtmlSetContent.prototype.uglifyWith = {
+  '.js' (text) {
+    const babelCode = babel.transform(text, {
+      extends: this.babelpath,
+    });
+
+    return uglifyJs(babelCode.code).code;
+  },
+
+  '.css' (text) {
+    return text;
+  },
+}
+
+HtmlSetContent.prototype.merges = function (html, text) {
+  let content = text;
+
+  const extname = path.extname(this.filepath);
+
+  if (this.uglify) {
+    content = this.uglifyWith[extname].call(this, text);
+  }
+
+  const intype = this.intype[extname];
+
+  return this.mergeText(this.inject, html, content, intype);
+};
+
+HtmlSetContent.prototype.apply = function (compiler) {
+  compiler.plugin('compilation', compilation => {
+    compilation.plugin('html-webpack-plugin-before-html-processing', (htmlPluginData, callback) => {
+      fs.readFile(this.filepath, 'utf8', (err, data) => {
+        if (err) reject(err);
+
+        htmlPluginData.html = this.merges(htmlPluginData.html, data);
+
+        callback(null, htmlPluginData);
+      });
+    });
+  });
+};
+
+module.exports = HtmlSetContent;
